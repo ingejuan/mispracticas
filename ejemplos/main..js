48fe0226ad9bc04f540ca2b523ecbe8342652b2f
ingejuan <ing.juanromero@gmail.com>
2022-10-05 14:59:37 -0300

    segundo commit 051022

---
+++ ejemplos/main..js
@@ -0,0 +1,22 @@
+//ejemplos de uso de switch
+
+let erica = (new Date().getDay());
+
+switch (erica){
+    case 0:
+        alert("es domingo");
+        break;
+    case 1:
+        alert("lunes");
+        break;
+    case 2:
+        alert("martes");
+        break;
+    case 3:
+        alert("es miercoles y se viene fon de semana de descanso")
+        break;
+    default:
+        alert("es otro dia")
+
+}
+
